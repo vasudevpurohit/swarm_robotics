49170325e166d752d47f4ed2744b242a7eb985c3
Vasudev Purohit <53868512+vasudevpurohit@users.noreply.github.com>
2021-09-24 20:30:17 -0400

    Further worked on tuning the controller
    -Added visualisation of the result on a 2D image
    -The controller needs to be tuned further, vehicle not following the trajectory

--- 01 Single Robot with RRTstar/RRTstar_planner.m
+++ 01 Single Robot with RRTstar/RRTstar_planner.m
@@ -31,24 +31,24 @@ inflate(map,t_width/2);
 stateSpace = stateSpaceSE2;
 stateValidator = validatorOccupancyMap(stateSpace);
 stateValidator.Map = map;
-stateValidator.ValidationDistance = 0.01; %checks the validity after every 0.01m of motion
+stateValidator.ValidationDistance = 5; %checks the validity after every 0.01m of motion
 stateSpace.StateBounds = [map.XWorldLimits; map.YWorldLimits;[-pi pi]];
 
 planner = plannerRRTStar(stateSpace,stateValidator);
 planner.ContinueAfterGoalReached = true;
 planner.MaxIterations = 80000;
-planner.MaxConnectionDistance = 0.15;
-planner.BallRadiusConstant = 100;
+planner.MaxConnectionDistance = 0.5;
+planner.BallRadiusConstant = 1;
 start = [50,10,0];
 goal  = [250,250,0];
 
 rng(100, 'twister')
 [pthObj, solnInfo] = plan(planner,start,goal);
 
-% map.show;
-% hold on;
-% plot(solnInfo.TreeData(:,1),solnInfo.TreeData(:,2), '.-'); % tree expansion
-% plot(pthObj.States(:,1),pthObj.States(:,2),'r-','LineWidth',2); % draw path
+map.show;
+hold on;
+plot(solnInfo.TreeData(:,1),solnInfo.TreeData(:,2), '.-'); % tree expansion
+plot(pthObj.States(:,1),pthObj.States(:,2),'r-','LineWidth',2); % draw path
 
 %%
 x = pthObj.States(:,1);
@@ -57,32 +57,47 @@ theta = pthObj.States(:,3);
 
 
 %calculating the distance travelled at every point on the path
+x_ref = [];
+y_ref = [];
+theta_ref = [];
 dist(1,1) = 0;
 for i = (1:1:length(x)-1)
     dist(i+1,1) = dist(i,1) + sqrt((x(i+1,1)-x(i,1))^2+(y(i+1,1)-y(i,1))^2);
 end
-x_ref = smooth(dist,x);
-y_ref = smooth(dist,y);
-theta_ref = smooth(dist,theta);
-X_ref = [dist x_ref];
-Y_ref = [dist y_ref];
-Theta_ref = [dist theta_ref];
+dist_ref = (linspace(0,dist(end,1),50))';
+x_ref = interp1(dist,x,dist_ref);
+x_cs = spline(dist_ref,x_ref);
+x_ref2 = ppval(x_cs,dist_ref);
+y_ref = interp1(dist,y,dist_ref);
+y_cs = spline(dist_ref,y_ref);
+y_ref2 = ppval(y_cs,dist_ref);
+theta_ref = interp1(dist,theta,dist_ref);
+theta_cs = spline(dist_ref,theta_ref);
+theta_ref2 = ppval(theta_cs,dist_ref);
+% X_ref = [dist x_ref];
+% Y_ref = [dist y_ref];
+% Theta_ref = [dist theta_ref];
 
-x_0 = x_ref(1,1);
-y_0 = y_ref(1,1);
-theta_0 = theta_ref(1,1);
+x_0 = x_ref2(1,1);
+y_0 = y_ref2(1,1);
+theta_0 = theta_ref2(1,1);
 
+% calculating the curvature at each point
+dx = gradient(x_ref2);
+d2x = gradient(dx);
+dy = gradient(y_ref2);
+d2y = gradient(dy);
+curvature = (dx.*d2y - dy.*d2x) ./(dx.^2+dy.^2).^(3/2);
 %% accessing the timeseries samples
-vizrate = rateControl(100);
-for i = (1:1:1457)
-    pose_out(i,1:3) = getdatasamples(out.currPose,i);
-    map.show;
-    hold on;
-    plotTrVec = [pose_out(1:2) 0];
-    plotRot = axang2quat([0 0 1 pose_out(3)]);
-    plotTransforms(plotTrVec, plotRot, 'MeshFilePath', 'groundvehicle.stl', 'Parent', gca, "View","2D", "FrameSize", frameSize);
-    waitfor(vizrate);
+vehicle_pose = zeros(length(out.tout),3);
+for i = (1:1:length(out.tout))
+    vehicle_pose(i,(1:3)) = getdatasamples(out.currPose,i);
 end
+map.show;
+hold on     
+plot(x_ref2,y_ref2,'r-');
+hold on
+plot(vehicle_pose(:,1),vehicle_pose(:,2),'b-');
 %%
 % %% creating a controller to follow the poses found through RRT*
 % maxLinvel = 15;      %max linear speed of the vehicle
